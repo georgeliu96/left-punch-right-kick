f42396fc0643846eb38eb67d926289abf7cbcf39
George Liu <georgeliu96@gmail.com>
2019-04-10 21:09:46 -0400

    Add animation for punching

--- left-punch-right-kick.js
+++ left-punch-right-kick.js
@@ -6,6 +6,14 @@ background.src = "left-punch-right-kick%20assets/Background.png";
 
 background.onload = () => {
     ctx.drawImage(background,0,0)
+    gameLoop();
+}
+
+function gameLoop() {
+
+    window.requestAnimationFrame(gameLoop)
+
+    plyrSprt.update();
     plyrSprt.render();
 }
 
@@ -14,7 +22,11 @@ var player = new Image();
 player.src = "left-punch-right-kick%20assets/player-sprite-4/adventurer-hand-combat-Sheet.png"
 
 function sprite(options) {
-    const that = {};
+    var that = {},
+        frameIndex = 0,
+        tickCount = 0,
+        ticksPerFrame = options.ticksPerFrame || 0;
+        numberOfFrames = options.numberOfFrames || 1;
 
     that.context = options.context.getContext("2d");
     that.height = options.height;
@@ -22,17 +34,38 @@ function sprite(options) {
     that.image = options.image;
 
     that.render = () => {
+
+        that.context.clearRect(0, 0, 900, 616);
+        ctx.drawImage(background,0,0)
+        that.context.scale(1.5,1.5);
         that.context.drawImage(
-            that.image, 0, 0, that.width, that.height, 450, 520, that.width, that.height
-        )
+            that.image, frameIndex * that.width, 0, that.width, that.height, 297, 335, that.width, that.height
+        );
+        that.context.scale(2/3, 2/3);
     }
+
+    that.loop = options.loop;
+
+    that.update = () => {
+        tickCount += 1;
+        if (tickCount > ticksPerFrame) {
+            tickCount = 0;
+            if (frameIndex < numberOfFrames - 1) {
+                frameIndex += 1;
+            } else {
+                frameIndex = 0;
+            }
+        }
+    };
 
     return that 
 }
 
 var plyrSprt = sprite({
     context: canvas,
-    height: 50,
-    width: 37,
-    image: player 
+    height: 37,
+    width: 50,
+    image: player,
+    numberOfFrames: 4,
+    ticksPerFrame: 10
 })
